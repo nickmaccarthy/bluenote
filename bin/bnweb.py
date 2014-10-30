ef651f0c221874bbb9cc653c31ea5e6d3214ab1b
Nick MacCarthy <nmaccarthy@shapeup.com>
2014-10-30 10:15:26 -0400

    Added gnunicor support bnweb

--- bin/bnweb.py
+++ bin/bnweb.py
@@ -29,7 +29,12 @@ from alerts import alerts
 from bn import bncfg 
 from bn_mail import mailcfg
 
+from flask import Flask
+from werkzeug.contrib.fixers import ProxyFix
+
 from app import app
+
+app.wsgi_app = ProxyFix(app.wsgi_app)
 
 if __name__ == "__main__":
     app.run()
